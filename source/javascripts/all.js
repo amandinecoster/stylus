5b5360e647782e0c3e89c4f7df5baec339678942
Amandine Coster <amandinecoster@gmail.com>
2015-08-27 16:08:10 +0200

    Scroll navbar
    Add style and jqry for a beautiful scrolling navbar

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,14 @@
 //= require jquery
 //= require bootstrap-sprockets
-//= require_tree .
+//= require_tree.
+
+$(document).ready(function(){
+
+  $(window).on("scroll", function(){
+    if ( $(this).scrollTop() > 80 ){
+      $(".navbar").css("background-color", "#555");
+      } else {
+          $(".navbar").css("background-color", "transparent");
+          }
+  });
+});
