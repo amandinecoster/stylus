1a70200c82508cc8ce6723411febd297fea765a7
Amandine Coster <amandinecoster@gmail.com>
2015-08-27 16:08:50 +0200

    Automated commit at 2015-08-27 14:08:50 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -12659,4 +12659,15 @@ return jQuery;
 
 
 
+//= require_tree.
 
+$(document).ready(function(){
+
+  $(window).on("scroll", function(){
+    if ( $(this).scrollTop() > 80 ){
+      $(".navbar").css("background-color", "#555");
+      } else {
+          $(".navbar").css("background-color", "transparent");
+          }
+  });
+});
